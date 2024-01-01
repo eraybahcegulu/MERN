e845550105a63b1e26ed1e0dfe72b2ba4265f048
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-01-01 19:20:16 +0300

    Update productRoutes.js

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productController = require("../controllers/productController");
 const auth = require('../middlewares/authMiddleware');
 
-router.get("/api/product/get-all", auth, productController.getAllProducts);
+router.get("/api/product/get-all", productController.getAllProducts);
 router.post("/api/product/add", auth, productController.addProduct);
 router.delete("/api/product/delete/:id", auth, productController.deleteProduct);
 router.put("/api/product/update/:id", auth, productController.updateProduct);
