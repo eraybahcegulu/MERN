d993fca24c79a8b77b2b5aa9c6bf545c18d46a94
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-01-17 22:53:49 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,28 +1,10 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const cookieParser = require('cookie-parser');
+const app = require('./app');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
 const db = require('./utils/connectDB')
 db.connect();
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({
-    origin: ["http://localhost:3000"],
-    credentials : true
-}));
-
-const routes = [
-    require('./routes/userRoutes'),
-    require('./routes/companyRoutes'),
-    require('./routes/productRoutes'),
-];
-
-routes.forEach(route => app.use(route));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
