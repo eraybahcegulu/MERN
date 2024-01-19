bf7db3509153a4fd42c28a9402d698bf4fd99034
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-01-19 22:48:30 +0300

    Update columns.tsx

--- client/src/components/Company/columns.tsx
+++ client/src/components/Company/columns.tsx
@@ -58,6 +58,7 @@ export const columns = (companyData: CompanyDataType[]) => {
         {
             title: 'Total Product',
             width: '25%',
+            sorter: (a, b) => a.products.length - b.products.length,
             render: (_, record) => (
                 <div className='flex flex-row'>
 
