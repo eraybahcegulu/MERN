7a1f6006006799960a4e68eabfbb0e5d8d81f02e
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-02-13 21:32:50 +0300

    mail template added

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -1,5 +1,4 @@
 const nodemailer = require("nodemailer");
-
 const responseHandler = require('../handlers/responseHandler')
 
 const transporter = nodemailer.createTransport({
@@ -12,17 +11,28 @@ const transporter = nodemailer.createTransport({
     },
 });
 
+const mailTemplate = (user, bodyText, buttonText, actionLink) => {
+    return `
+        <div style="background-color: lightgrey; padding: 20px; border-radius: 10px; font-family: Arial, sans-serif; font-size: 16px; color: black; line-height: 1.6;">
+            <p>Hello, <strong>${user}</strong></p>
+            <p>${bodyText}</p>
+            <a href="${actionLink}" style="display: inline-block; background-color: blue; color: white; text-decoration: none; padding: 10px 20px; border-radius: 5px;">${buttonText}</a>
+        </div>
+    `;
+};
+
 const sendMailEmailConfirm = async (userData) => {
     const { userName, email, emailConfirmToken } = userData;
 
     let mailOptions = {
         to: email,
         subject: 'Confirm Email',
-        html:
-            `
-            <p> Hello, ${userName} </p>
-            <a href="${process.env.CLIENT_URL}/api/user/emailConfirm/${emailConfirmToken}"> Confirm Email </a>
-            `
+        html: mailTemplate(
+            userName,
+            'Please click the link below to confirm your email address.',
+            'Confirm Email',
+            `${process.env.CLIENT_URL}/api/user/emailConfirm/${emailConfirmToken}`
+        )
     };
 
     try {
@@ -40,11 +50,12 @@ const sendMailChangeEmailConfirm = async (userData) => {
     let mailOptions = {
         to: email,
         subject: 'Confirm Change Email',
-        html:
-            `
-            <p> Hello, ${userName} </p>
-            <a href="${process.env.CLIENT_URL}/api/user/changeEmailConfirm/${changeEmailConfirmToken}"> Confirm Change Email </a>
-            `
+        html: mailTemplate(
+            userName,
+            'Please click the link below to confirm the change of your email address.',
+            'Confirm Change Email',
+            `${process.env.CLIENT_URL}/api/user/changeEmailConfirm/${changeEmailConfirmToken}`
+        )
     };
 
     try {
@@ -61,12 +72,13 @@ const sendMailInvalidLoginAttempt = async (userData) => {
 
     let mailOptions = {
         to: email,
-        subject: `5 Invalid login attempts have been made using ${email} `,
-        html:
-            `
-            <p> Hello </p>
-            <a href="${process.env.CLIENT_URL}"> ${invalidLoginAttempt} Invalid Login Attempts! </a>
-            `
+        subject: `${invalidLoginAttempt} invalid login attempts have been made using ${email} `,
+        html: mailTemplate(
+            email,
+            `There have been ${invalidLoginAttempt} invalid login attempts using your email address.`,
+            'Visit Website',
+            `${process.env.CLIENT_URL}`
+        )
     };
 
     try {
@@ -84,11 +96,12 @@ const sendMailForgotPassword = async (userData) => {
     let mailOptions = {
         to: email,
         subject: `Forgot Password ${email}`,
-        html:
-            `
-            <p> Hello, ${userName} </p>
-            <a href="${process.env.CLIENT_URL}/api/user/resetPassword/${resetPasswordToken}"> Reset Password </a>
-            `
+        html: mailTemplate(
+            userName,
+            'You have requested to reset your password. If you did not request this, please ignore this email.<br>Please click the link below to reset your password.',
+            'Reset Password',
+            `${process.env.CLIENT_URL}/api/user/resetPassword/${resetPasswordToken}`
+        )
     };
 
     try {
