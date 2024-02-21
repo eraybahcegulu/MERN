15e39015985cf8d9cadfd47fad2d08f4c06fb7b9
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-02-21 17:55:31 +0300

    Update LoginForm.tsx

--- client/src/components/Login/LoginForm.tsx
+++ client/src/components/Login/LoginForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GithubOutlined, GoogleOutlined, InfoCircleOutlined, LockOutlined, TwitterOutlined, UserOutlined } from '@ant-design/icons';
+import { GithubOutlined, GoogleOutlined, InfoCircleOutlined, LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Form, Input, Popover, Switch } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDiscord } from '@fortawesome/free-brands-svg-icons'
